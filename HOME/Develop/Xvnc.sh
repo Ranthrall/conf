897cef31b19520fd728d1bad71f9b4fb6806c70f
Fernando Manfredi <blackice@craft.net.br>
2014-06-30 23:46:25 -0300

    Atualizando... 2

--- HOME/Develop/Xvnc.sh
+++ HOME/Develop/Xvnc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-x0vncserver -AAAAwordFile=/home/blackice/.vnc/AAAAwd -AcceptPointerEvents=off -QueryConnect=on > /home/blackice/Xvnc.log 2>&1 &
+x0vncserver -passwordFile=/home/blackice/.vnc/passwd -AcceptPointerEvents=off -QueryConnect=on > /home/blackice/Xvnc.log 2>&1 &
 
